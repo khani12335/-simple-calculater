c9be4437a2cb8c0df96e5192c829fe486f70143e
dani_khan <159428334+khani12335@users.noreply.github.com>
2024-04-18 17:05:59 +0500

    fisrt_commit

---
+++ index.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter first number", type: "number", name: "firstnumber" },
+    { message: "enter second number", type: "number", name: "secondnumber" },
+    {
+        message: "select one of the operator to perfoms operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subraction", "Multiplication", "Division"]
+    },
+]);
+//conditional statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "Subraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("please slaect a valid operator:");
+}
